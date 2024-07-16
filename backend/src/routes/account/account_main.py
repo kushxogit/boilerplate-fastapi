0a99eeafd22ed46cdb16a0ad79bbaf43f6358d88
pateloncode <formalmanan@gmail.com>
2024-07-16 14:13:08 +0530

    error resolved

--- backend/src/routes/account/account_main.py
+++ backend/src/routes/account/account_main.py
@@ -1,10 +1,10 @@
-from fastapi import FastAPI
-from .rest_api.account_controller import router as account_router
 
-app = FastAPI()
+from fastapi import APIRouter
+from .rest_api.account_controller import signup
 
-app.include_router(account_router)
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+router = APIRouter(prefix="/accounts", tags=["Account"])
+
+router.post("/",response_model=dict)(signup)
+
+
